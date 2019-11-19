a78b15d8ec700b7f27f41257e6d3819002854caa
Ben Brennan <brennben@umich.edu>
2019-11-19 12:02:18 -0500

    added to_predict and updated the vignette

--- R/linear.R
+++ R/linear.R
@@ -6,38 +6,34 @@
 #' @param y
 #' n by 1 vector of numeric continuous outcomes
 #' @param need_intercept
-#' do you need us to add an intercept column to your design matrix? we assume TRUE
+#' do you need to add an intercept column to your design matrix? we assume TRUE
 #'
 #' @examples
+#'
 #' data(mtcars)
 #' y = mtcars$mpg
 #' X = mtcars$cyl
+#' # can also make X a matrix, no problem
 #' fit = linear(X,y)
 #' betas = fit$coeffs
 #'
 #' @export
 
-linear = function(X,y,need_intercept = T){
+linear = function(X, y, need_intercept = T , to_predict = NULL){
+
+
 
   if(is.null(dim(X))){
+
     n = length(X)
-    p = 1
-  } else{
+
+
+  } else {
+
     n = dim(X)[1]
-    p = dim(X)[2]
+
   }
 
-  if(n != length(y)){
-    if(n < length(y)){
-      stop('You are missing some rows in your design matrix')
-    } else {
-      stop('You need less outcomes')
-    }
-  }
-
-  if(n <= p){
-    stop('Too many covariates for simple OLS, bro')
-  }
 
   if(need_intercept){
 
@@ -45,26 +41,86 @@ linear = function(X,y,need_intercept = T){
 
   }
 
-  betas = solve(t(X)%*%X) %*% (t(X)%*%y)
+  p = dim(X)[2]
+
+
+  if(n != length(y)){
+
+    if(n < length(y)){
+
+      stop('You are missing some rows in your design matrix')
+
+    } else {
+
+      stop('You need less outcomes')
+
+    }
+
+  }
+  if(n <= p){
+
+    stop('Too many covariates for simple OLS')
+
+  }
+
+  Xt_X_inv = solve(t(X)%*%X)
+  betas = Xt_X_inv %*% (t(X)%*%y)
   fitted = X %*% betas
   residuals = fitted - y
-  mse = (n-p-1)^-1 * sum(residuals^2)
-  var_cov = mse * solve(t(X)%*%X)
+  mse = sum(residuals^2)/(n-p-1)
+  var_cov = mse * Xt_X_inv
   beta_var = diag(var_cov)
-
   t_stats = betas/sqrt(beta_var)
-  p = dt(t_stats,n-p-1)
-  crit_value = qt(0.975, n-p)
-  conclusion = ifelse( abs(t_stats) > crit_value, 'Reject', 'Fail to Reject' )
+  crit_value = qt(0.975, n-p-1)
+  conclusion = ifelse( abs(t_stats) > crit_value, 'Reject', 'Fail to Reject')
+
+
+  if(!is.null(to_predict)){
+
+
+    if(all(dim(to_predict) == c(n,p))){
+
+      warning('Predictions will be wrong if your prediction data is not in the same order as your design')
+
+      predicted =  to_predict %*% betas
+
+    } else if (all(dim(to_predict) == c(n,p-1))){
+
+      warning('Predictions will be wrong if your prediction data is not in the same order as your design')
+
+      warning('Adding intercept to prediction')
+
+      to_predict = cbind(rep(1,n),to_predict)
+
+      predicted =  to_predict %*% betas
+    }
+
+    else (
+
+      stop('Design matrix to be predicted does not have the correct dimensions')
+
+    )
+
+  } else {
+
+    predicted = NULL
+
+  }
 
   rtrn = list(coeffs = betas,
+
               fitted_values = fitted,
+
               se = sqrt(beta_var),
+
               res = residuals,
-              varcov = var_cov,
+
+              predicted = predicted,
+
               t = t_stats,
-              p = p,
+
               conclusions = conclusion
               )
+
   return(rtrn)
 }
