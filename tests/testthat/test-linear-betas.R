e55e185be4af34f412f552bec8e0082dc2fdc8cb
Ben Brennan <brennben@umich.edu>
2019-11-20 13:33:43 -0500

    added test cases!

---
+++ tests/testthat/test-linear-betas.R
@@ -0,0 +1,21 @@
+context('linear')
+
+test_that("i get the correct coefficients for linear", {
+  X = matrix(rnorm(100,sd = 10),100,1)
+  X1 = matrix(rnorm(1000),100,10)
+  X_wrong = matrix(rnorm(100),100,10)
+  y = rnorm(100, sd = 4)
+  y_wrong = rnorm(99, sd = 4)
+
+  expect_equal(as.vector(lm(y ~ X)$coefficients), as.vector(linear(X,y)$coeffs))
+  expect_equal(as.vector(lm(y ~ X1)$coefficients), as.vector(linear(X1,y)$coeffs))
+
+  expect_equal(as.vector(lm(y ~ X)$fitted.values), as.vector(linear(X,y)$fitted_values))
+  expect_equal(as.vector(lm(y ~ X1)$fitted.values), as.vector(linear(X1,y)$fitted_values))
+
+  expect_equal(abs(as.vector(lm(y ~ X)$residuals)), abs(as.vector(linear(X,y)$res)))
+  expect_equal(abs(as.vector(lm(y ~ X1)$residuals)), abs(as.vector(linear(X1,y)$res)))
+
+  expect_error(linear(X,y_wrong),'You need more outcomes')
+  expect_error(linear(X_wrong,y), 'X must be full column-rank')
+})
---
+++ tests/testthat/test-logistic.R
@@ -0,0 +1,14 @@
+context('logistic')
+
+test_that("logistic works", {
+  X = matrix(rnorm(100,sd = 10),100,1)
+  X1 = matrix(rnorm(1000),100,10)
+
+  y = rbinom(100,1, 0.3)
+  y_wrong = sample(1:9,100, replace = T)
+
+  expect_equal(as.vector(glm(y~X, family= 'binomial')$coefficients), as.vector(logistic(X,y)$coeffs))
+  expect_equal(as.vector(glm(y~X1, family= 'binomial')$coefficients), as.vector(logistic(X1,y)$coeffs))
+
+  expect_error(logistic(X,y_wrong), 'Wrong type of data')
+})
