316ca219139ce0834fbc7ff2fd4f881f8d42ba0a
Ben Brennan <brennben@umich.edu>
2019-11-20 22:27:15 -0500

    i swear to god...

--- tests/testthat/test-logistic.R
+++ tests/testthat/test-logistic.R
@@ -3,7 +3,7 @@ context('logistic')
 test_that("logistic works", {
   X = matrix(rnorm(100,sd = 10),100,1)
   X1 = matrix(rnorm(1000),100,10)
-
+  x_vec = sample(1:20, 100,replace = T)
   y = rbinom(100,1, 0.3)
   y_wrong = sample(1:9,100, replace = T)
 
@@ -11,6 +11,8 @@ test_that("logistic works", {
 
   expect_equal(as.vector(glm(y~X, family= 'binomial')$coefficients), as.vector(logistic(X,y)$coeffs))
   expect_equal(as.vector(glm(y~X1, family= 'binomial')$coefficients),  as.vector(logistic(X1,y)$coeffs))
+  expect_equal(as.vector(glm(y~x_vec, family= 'binomial')$coefficients),
+               as.vector(logistic(x_vec,y)$coeffs))
 
   expect_error(logistic(X,y_wrong), 'Wrong type of data')
   expect_error(logistic(X,y, i_max = 1), "IRWLS failed to converge")
