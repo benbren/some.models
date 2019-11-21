2d1e74b4aac3d4ece55edca16aecdd155422bd9a
Ben Brennan <brennben@umich.edu>
2019-11-20 21:27:10 -0500

    added test cases for logistic

--- tests/testthat/test-logistic.R
+++ tests/testthat/test-logistic.R
@@ -14,4 +14,14 @@ test_that("logistic works", {
 
   expect_error(logistic(X,y_wrong), 'Wrong type of data')
   expect_error(logistic(X,y, i_max = 1), "IRWLS failed to converge")
+  expect_error(logistic(X,y,to_predict = c(12,1,3)),'Cant predict - incorrect # of covariates')
+  expect_error(logistic(X,y,to_predict = matrix(rnorm(100*100),100,100)),'Cant predict - incorrect # of covariates')
+
+  expect_warning(logistic(X,y, to_predict = 4), 'Prediction wrong if to_predict not in same order as design matrix - adding intercept')
+  expect_warning(logistic(X,y, to_predict = c(1,4)), 'Prediction wrong if to_predict not in same order as design matrix')
+
+  expect_warning(logistic(X1,y, to_predict = sample(1:10,10)), 'Prediction wrong if to_predict not in same order as design matrix - adding intercept')
+  expect_warning(logistic(X,y, to_predict = c(1,4)), 'Prediction wrong if to_predict not in same order as design matrix')
+
+
 })
